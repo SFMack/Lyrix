c1bb5a5a2b35e76ca294498e8c08cf4ba774faef
SFMack <sfcamack@icloud.com>
2020-11-13 20:00:52 -0800

    crud updates

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -13,11 +15,13 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL = `mongodb+srv://admin:Chr1st!an@cluster0.haxjt.mongodb.net/<dbname>?retryWrites=true&w=majority`;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server running on port: ${PORT}`);
